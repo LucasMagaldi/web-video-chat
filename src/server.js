9fba552f8aa11df5c42977cfc25d77978791bb60
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-10-05 11:19:49 -0300

    make socket io script available to client

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
 const app = express()
 const server = createServer (app)
 const io = new Server(server)
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const appPath = __dirname + '/app/index.html'
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Server')
+    res.sendFile(join(appPath))
 })
 
 io.on('connection', (socket) => {
